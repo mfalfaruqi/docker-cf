21265c4e425c8e072ad342c6d7a2f3102e4139fa
Muhammad Faisal Alfaruqi <mfalfaruqi@gmail.com>
2017-08-19 09:17:03 +0700

    initial commit

---
+++ www/default/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Local</title>
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="<?= $projroot ?>css/main.css">
+    </head>
+
+    <body>
+    	Server 1
+    	<?php phpinfo(); ?>
+    </body>
+</html>
